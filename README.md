eb40c48bdd7b39eb253d076bee09b6de1d181e13
Chandler00 <xinyufan00@gmail.com>
2021-08-17 23:31:15 -0400

    2 lambda functions added

---
+++ README.md
@@ -0,0 +1,13 @@
+## Serverless online transaction backend process
+
+
+### Logicflow
+
+Lambda Event types: 
+1. Purchase
+2. Refund
+
+Terminal:
+Statemachine
+
+
---
+++ lambdas/refund.py
@@ -0,0 +1,16 @@
+import datetime
+
+
+def refund(message, context):
+
+    # input example
+    # {'TransactionType': 'PURCHASE'}
+    # log the message
+    print('Received message from step functions {message}'.format(message=message))
+
+    # 2 response
+    response = {'TransType': message['TransactionType']
+                , 'Timestamp': datetime.datetime.now().strftime("%Y-%m-%d %H-%M-%S")
+                , 'Message': 'lambda processing the purchase'}
+
+    return response
